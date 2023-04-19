6b7275cc64d9486bdbadaadd8b9c9408e91b0758
Juvenal Carlos Cardoso <juvenalcardoso961@gmail.com>
2023-04-19 13:17:59 -0300

    Criando métodos para fazer buscas especificas

--- projeto/src/main/java/br/com/web/projetoweb/repository/ConvidadoRepository.java
+++ projeto/src/main/java/br/com/web/projetoweb/repository/ConvidadoRepository.java
@@ -3,7 +3,10 @@ package br.com.web.projetoweb.repository;
 import org.springframework.data.repository.CrudRepository;
 
 import br.com.web.projetoweb.models.Convidado;
+import br.com.web.projetoweb.models.Evento;
 
 public interface  ConvidadoRepository extends  CrudRepository<Convidado, String> {
+	
+	Iterable<Convidado> findByEvento(Evento evento);
     
 }
